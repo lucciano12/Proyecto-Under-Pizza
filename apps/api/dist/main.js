1896dc961b677a6c6f52b35697ea25e3d37899df
Lucciano99 <lpradoe@hotmail.com>
2026-03-11 13:37:03 -0300

    se corrigue error ;

--- apps/api/dist/main.js
+++ apps/api/dist/main.js
@@ -20,7 +20,7 @@ async function bootstrap() {
         .addTag('categorías', 'Gestión de categorías')
         .addTag('movimientos', 'Registro de movimientos de productos')
         .addServer('http://localhost:3000', 'Desarrollo local')
-        .addServer('http://3.17.152.56:3000', 'Produccion EC2')
+        .addServer('https://3.17.152.56:3000', 'Produccion EC2')
         .build();
     const document = swagger_1.SwaggerModule.createDocument(app, config);
     swagger_1.SwaggerModule.setup('api/docs', app, document, {
@@ -35,7 +35,7 @@ async function bootstrap() {
     await app.listen(port);
     console.log(`\n🚀 API corriendo en: http://localhost:${port}`);
     console.log(`📚 Swagger Docs: http://localhost:${port}/api/docs\n`);
-    console.log((`📚 Swagger Docs: http://3.17.152.56:${port}/api/docs\n`));
+    console.log(`📚 Swagger Docs: http://3.17.152.56:${port}/api/docs\n`);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
